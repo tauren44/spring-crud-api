d8cc309cbb48b5a7112f202b927d0dd48338fec5
tauren44 <tauren4j@gmail.com>
2019-06-02 17:32:37 +0300

    added security

---
+++ src/main/java/com/example/springdemo/security/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.springdemo.security;
+
+import com.example.springdemo.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserService userService;
+
+    @Override
+    public UserDetailsImpl loadUserByUsername(String email) throws UsernameNotFoundException {
+        return new UserDetailsImpl(userService.findByEmail(email));
+    }
+}
